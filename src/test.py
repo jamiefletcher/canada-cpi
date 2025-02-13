ed9fb853a4b0105941e18bd2de0f679abac67f24
Jamie Fletcher <68712911+jamiefletcher@users.noreply.github.com>
2025-02-13 18:16:35 -0500

    Experiment with getting datacube from StatCan REST API

--- src/test.py
+++ src/test.py
@@ -1,11 +1,12 @@
 import requests
-
+import json
 
 def statcan_get_metadata(pid: int):
     url = "https://www150.statcan.gc.ca/t1/wds/rest/getCubeMetadata"
     payload = [{"productId": pid}]
     headers = {"Content-Type": "application/json"}
     r = requests.post(url, json=payload, headers=headers)
+    r.raise_for_status()
     r_json = r.json()
     metadata = {}
     if r_json[0]["status"] == "SUCCESS":
@@ -13,13 +14,29 @@ def statcan_get_metadata(pid: int):
             metadata[field] = r_json[0]["object"][field]
     return metadata
 
+def statcan_get_data(pid: int, coordinate: str, periods: int):
+    url = "https://www150.statcan.gc.ca/t1/wds/rest/getDataFromCubePidCoordAndLatestNPeriods"
+    payload = [{"productId": pid, "coordinate": coordinate, "latestN": periods}]
+    headers = {"Content-Type": "application/json"}
+    r = requests.post(url, json=payload, headers=headers)
+    r.raise_for_status()
+    r_json = r.json()
+    print(r_json)
+
 
 def main():
-    metadata = statcan_get_metadata(pid=18100004)
-    for dim in metadata["dimension"]:
-        print(dim["dimensionNameEn"], "# members:", len(dim["member"]))
-        # for member in dim["member"]:
-        #     print("  ", member["memberId"], member["memberNameEn"])
+    cpi_pid = 18100004
+    
+    metadata = statcan_get_metadata(pid=cpi_pid)
+    with open(f"data/{cpi_pid}_metadata.json", "w") as f:
+        json.dump(metadata, f, indent=4, ensure_ascii=False)
+    
+    for d in metadata["dimension"]:
+        print(d["dimensionNameEn"])
+        mems = [(m["memberId"], m["parentMemberId"], m["memberNameEn"]) for m in d["member"]]
+        print(mems)
+
+    # statcan_get_data(pid=18100004, coordinate="2.2.0.0.0.0.0.0.0.0", periods=12)
 
 
 if __name__ == "__main__":
