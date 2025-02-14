88e4f07833c1bf87740177b2cfd8924eb5a7d1c4
Jamie Fletcher <68712911+jamiefletcher@users.noreply.github.com>
2025-02-14 16:19:33 -0500

    Factor out metadata processing into private method

--- src/test.py
+++ src/test.py
@@ -1,35 +1,44 @@
 import requests
 import json
 
-def statcan_get_metadata(pid: int):
+def statcan_get_metadata(pid: int, save_file = ""):
     url = "https://www150.statcan.gc.ca/t1/wds/rest/getCubeMetadata"
     payload = [{"productId": pid}]
     headers = {"Content-Type": "application/json"}
     r = requests.post(url, json=payload, headers=headers)
     r.raise_for_status()
-    r_json = r.json()
-    metadata = {}
+    r_json = r.json()    
     if r_json[0]["status"] == "SUCCESS":
-        for field in ["productId", "cansimId", "cubeTitleEn", "cubeStartDate", "cubeEndDate"]:
-            metadata[field] = r_json[0]["object"][field]
+        metadata = _process_metadata(r_json)
+    if save_file:
+        with open(save_file, "w") as f:
+            json.dump(metadata, f, indent=4, ensure_ascii=False)
+    return metadata
 
-        metadata["dimension"] = {}
-        for d in r_json[0]["object"]["dimension"]:
-            dim_id = int(d["dimensionPositionId"])
-            dim_name = d["dimensionNameEn"]
-            members = {}
-            for m in d["member"]:
-                m_id = int(m["memberId"])
-                parent_id = int(m["parentMemberId"]) if m["parentMemberId"] else None
-                members[m_id] = {
-                    "children" : [],
-                    "name" : m["memberNameEn"]
-                }
-                if parent_id:
-                    members[parent_id]["children"].append(m_id)
+def _process_metadata(r_json: dict):
+    metadata = {}
+    for field in ["productId", "cansimId", "cubeTitleEn", "cubeStartDate", "cubeEndDate"]:
+        metadata[field] = r_json[0]["object"][field]
 
-            metadata["dimension"][dim_id] = [dim_name, members]
-            
+    metadata["dimension"] = {}
+    for d in r_json[0]["object"]["dimension"]:
+        dim_id = int(d["dimensionPositionId"])
+        dim_name = d["dimensionNameEn"]
+        members = {}
+        for m in d["member"]:
+            m_id = int(m["memberId"])
+            if m_id not in members:
+                members[m_id] = {"children" : []}
+            members[m_id].update({"name" : m["memberNameEn"]})
+            parent_id = int(m["parentMemberId"]) if m["parentMemberId"] else None
+            if parent_id:
+                if parent_id not in members:
+                    members[parent_id] = {"children" : []}
+                members[parent_id]["children"].append(m_id)
+        metadata["dimension"][dim_id] = {
+            "name" : dim_name, 
+            "members" : members
+        }
     return metadata
 
 def statcan_get_data(pid: int, coordinate: str, periods: int):
@@ -45,11 +54,12 @@ def statcan_get_data(pid: int, coordinate: str, periods: int):
 def main():
     cpi_pid = 18100004
     
-    metadata = statcan_get_metadata(pid=cpi_pid)
+    metadata = statcan_get_metadata(
+        pid=cpi_pid, save_file=f"data/{cpi_pid}_metadata.json"
+    )
 
     print(metadata)
-    with open(f"data/{cpi_pid}_metadata.json", "w") as f:
-        json.dump(metadata, f, indent=4, ensure_ascii=False)
+
     
     # for d in metadata["dimension"]:
     #     print(d["dimensionNameEn"])
